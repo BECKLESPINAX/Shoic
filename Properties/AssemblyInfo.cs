8e083216e5c455f1aabd2068b1211bcaeca50173
BECKLESPINAX <germanosaurio@hotmail.com>
2024-10-11 18:02:24 -0300

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Super High Orbit Ion Cannon")]
+[assembly: AssemblyDescription("TCP/IP Stress Herramienta")]
+[assembly: AssemblyProduct("Super Hight Orbit Ion Cannon Santa Fe")]
+[assembly: AssemblyCopyright("Dominio Público")]
+
+[assembly: System.Resources.NeutralResourcesLanguage("es-AR")]
+
+[assembly: System.CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("312adafc-fdac-484b-84c5-5c5457e47f67")]
+
+[assembly: AssemblyVersion("1.0.8.0")]
